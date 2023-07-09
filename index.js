4ab63d730b28cf8cd3823563b14912f4e5e09f10
shivkumarojha <ojhashivkumar88@gmail.com>
2023-07-09 22:08:07 +0530

    added POST route handler to add new task in todo app

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+// Imports
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+// Importing uuid for uniquely give id to each task
+const { v4: uuidv4 } = require('uuid')
+
+// Port 
 const PORT = '3000'
+
+// MiddleWare for bodyParser
+app.use(bodyParser.json())
 
 // Array of objects that will contain each task 
 const tasks = []
 
 // Get request handler for getting every todo element
-app.get('/tasks', (req, res) => {
-    res.status(200).json(tasks)
+app.get('/api/v1/tasks', (req, res) => {
+    res.status(200). json(tasks)
+})
+
+
+// Creating a new task in todo app
+app.post('/api/v1/tasks', (req, res) => {
+    const title = req.body.title
+    const details = req.body.details
+    const date = req.body.date
+    const time = req.body.time
+    const repeat = req.body.repeat
+
+    task = {
+        id: uuidv4(),
+        title: title,
+        details: details,
+        date: date,
+        time: time,
+        repeat: repeat
+    }
+    tasks.push(task)
+    res.status(201).json(task)
+
 })
 
 
